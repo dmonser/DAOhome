cc3d7469e3a830757b8c15be4bc922a96ceb72ec
Роман Носов <romannosov_2001@mail.ru>
2023-10-03 10:32:13 +0300

    Initial commit

---
+++ src/main/java/ru/netology/DAOhome/DaOhomeApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.DAOhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaOhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaOhomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/DAOhome/DaOhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.DAOhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaOhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
